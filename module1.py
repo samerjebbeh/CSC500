8b0051f48a1f3b07c92d85b1779763844ab5f22b
Samer Jebbeh <samerjebbeh@gmail.com>
2025-09-12 20:06:53 -0400

    Completed python file as per instructions

--- module1.py
+++ module1.py
@@ -1,1 +1,13 @@
-print('hello world!')
+#Gather inputs for num1 and num2
+print("Input the first number")
+num1 = int(input())
+print("Input the second number")
+num2 = int(input())
+
+#Part 1
+print("The sum of the two numbers is:",num1+num2)
+print("The difference of the two numbers is:",num1-num2)
+
+#Part 2
+print("The multiplication of the two numbers is:",num1*num2)
+print("The division of the two numbers is:",num1/num2)
